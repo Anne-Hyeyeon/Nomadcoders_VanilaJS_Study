750b2f26444e60acdf5c90f4af2e7f32e9764d58
Anne-Hyeyeon <khy2106@naver.com>
2022-06-24 23:57:47 +0900

    3강 start

---
+++ 03/app.js
@@ -0,0 +1,27 @@
+// const title = document.getElementById("title");
+
+// console.dir(title);
+
+// title.innerText = "Got you!";
+
+// console.log(title.id);
+// console.log(title.className)
+
+
+// const princess = document. getElementsByClassName("princess");
+
+
+// console.log(princess);
+
+//h1 가져오기
+
+// const title = document.getElementsByTagName("h1");
+
+// console.log(title);
+// //이 경우, title이 array로 나온다. 우리의 최종 목적이 아님.
+
+// tag -> h1, div, span 등
+
+const title = document.querySelector(".princess h1");
+
+console.log(title);
